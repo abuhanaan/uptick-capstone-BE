51e6da876ed4ff4ded3009ba0e726d701ec95b77
Yuusuf Abdullahi <yusufabdullahi2910@gmail.com>
2023-10-31 21:19:45 +0100

    Hero & Mission components

---
+++ frontend/src/app/about/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Link from 'next/link';
+
+export default function () {
+  return (
+    <div>
+      <Link href="/contact-us"> Contact Us</Link>
+    </div>
+  )
+}
