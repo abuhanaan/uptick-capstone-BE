d3d85d0874e5e556fb3a7308fccd25dffa159282
DESTINYAS <destiny.ochonogor@gmail.com>
2023-11-11 23:05:59 +0100

    adding validationPipe to main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,32 @@
-import { NestFactory } from '@nestjs/core';
+import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
+import { NestFactory, Reflector } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder,SwaggerDocumentOptions } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.useGlobalPipes(new ValidationPipe({}))
+
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(
+    app.get(Reflector))
+  );
+  const config = new DocumentBuilder()
+    .setTitle('Uptick Fellowship - Team A')
+    .setDescription('The Capstone Project API description')
+    .setVersion('0.1')
+    .addBearerAuth()
+    .build();
+
+    const options: SwaggerDocumentOptions = {
+      operationIdFactory: (controllerKey: string, methodKey: string) => methodKey,
+    };
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
 }
 bootstrap();
