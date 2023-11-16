29eeee7867bed142233a325b269382ddce8f6522
Barny Victor <victorbarny4@gmail.com>
2023-11-16 15:47:41 +0100

    fixed conflicts from main.ts and environment variables

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,17 +1,27 @@
-import { NestFactory } from '@nestjs/core';
+import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
+import { NestFactory, Reflector } from '@nestjs/core';
+import {
+  SwaggerModule,
+  DocumentBuilder,
+  SwaggerDocumentOptions,
+} from '@nestjs/swagger';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      forbidNonWhitelisted: false,
-    }),
-  );
-  app.setGlobalPrefix('api/v1');
+
+  app.useGlobalPipes(new ValidationPipe({}));
+
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+  const config = new DocumentBuilder()
+    .setTitle('Uptick Fellowship - Team A')
+    .setDescription('The Capstone Project API description')
+    .setVersion('0.1')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
