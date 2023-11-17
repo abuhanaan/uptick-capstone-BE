c48b2af2311709b8cfad3cb5285913fc04f0daab
abuhanaan <adewunmi99@gmail.com>
2023-11-17 20:37:00 +0100

    Updated Schema

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,17 +1,18 @@
 import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 import { NestFactory, Reflector } from '@nestjs/core';
-import { SwaggerModule, DocumentBuilder,SwaggerDocumentOptions } from '@nestjs/swagger';
+import {
+  SwaggerModule,
+  DocumentBuilder,
+  SwaggerDocumentOptions,
+} from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  
   const app = await NestFactory.create(AppModule);
 
-  app.useGlobalPipes(new ValidationPipe({}))
+  app.useGlobalPipes(new ValidationPipe({}));
 
-  app.useGlobalInterceptors(new ClassSerializerInterceptor(
-    app.get(Reflector))
-  );
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
   const config = new DocumentBuilder()
     .setTitle('Uptick Fellowship - Team A')
     .setDescription('The Capstone Project API description')
@@ -20,6 +21,8 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
+
+  app.enableCors();
 
   await app.listen(3000);
 }
