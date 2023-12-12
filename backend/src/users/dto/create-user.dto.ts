e0b45c83f98600983370a7e565a511b81e280a48
abuhanaan <adewunmi99@gmail.com>
2023-12-12 13:04:07 +0100

    Added dummy endpoint for test purpose

--- backend/src/users/dto/create-user.dto.ts
+++ backend/src/users/dto/create-user.dto.ts
@@ -1,1 +1,16 @@
-export class CreateUserDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty } from 'class-validator';
+
+export class CreateUserDto {
+  @IsNotEmpty()
+  @ApiProperty()
+  username: string;
+
+  @IsNotEmpty()
+  @ApiProperty()
+  role: string;
+
+  @IsNotEmpty()
+  @ApiProperty()
+  password: string;
+}
